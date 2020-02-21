27ab525efb15ea1d96e7761bd090c6fb423e6aef
Deepak Bhardwaj <deepak@customersuccessbox.com>
2020-02-21 17:27:45 +0530

    initial commit

---
+++ src/main/java/com/tariffsystem/demo/TariffSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tariffsystem.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TariffSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TariffSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tariffsystem/demo/TariffSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tariffsystem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TariffSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
